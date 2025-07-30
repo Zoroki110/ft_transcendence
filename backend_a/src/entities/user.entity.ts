157cb9f62f776e1f8d7e55a20e24d1ce2365bf98
Zoroki110 <toiki.rouillard22@gmail.com>
2025-07-30 16:33:46 +0200

    feat(users): basic CRUD implemented with TypeORM + tested on Postman (GET/POST)

---
+++ backend_a/src/entities/user.entity.ts
@@ -0,0 +1,27 @@
+import { Entity, PrimaryGeneratedColumn, Column, OneToMany } from 'typeorm';
+import { Match } from './match.entity';
+import { ChatMessage } from './chat-message.entity';
+
+@Entity()
+export class User {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({ unique: true })
+  username: string;
+
+  @Column({ unique: true })
+  email: string;
+
+  @Column()
+  password: string;
+
+  @OneToMany(() => Match, match => match.player1)
+  matchesAsPlayer1: Match[];
+
+  @OneToMany(() => Match, match => match.player2)
+  matchesAsPlayer2: Match[];
+
+  @OneToMany(() => ChatMessage, message => message.sender)
+  messages: ChatMessage[];
+}
