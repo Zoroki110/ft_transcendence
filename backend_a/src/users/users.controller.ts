ce5f39b9210b120ade85e2e27d3372d1c4f71d90
Zoroki110 <toiki.rouillard22@gmail.com>
2025-08-18 10:38:26 +0200

    all

--- backend_a/src/users/users.controller.ts
+++ backend_a/src/users/users.controller.ts
@@ -1,45 +1,45 @@
 import {
-    Controller,
-    Get,
-    Post,
-    Body,
-    Param,
-    Put,
-    Delete,
-  } from '@nestjs/common';
-  import { UsersService } from './users.service';
-  import { CreateUserDto } from './dto/create-user.dto';
-  import { UpdateUserDto } from './dto/update-user.dto';
-  import { ApiTags } from '@nestjs/swagger';
-  
-  @ApiTags('users')
-  @Controller('users')
-  export class UsersController {
-    constructor(private readonly usersService: UsersService) {}
-  
-    @Get()
-    findAll() {
-      return this.usersService.findAll();
-    }
-  
-    @Get(':id')
-    findOne(@Param('id') id: string) {
-      return this.usersService.findOne(+id);
-    }
-  
-    @Post()
-    create(@Body() dto: CreateUserDto) {
-      return this.usersService.create(dto);
-    }
-  
-    @Put(':id')
-    update(@Param('id') id: string, @Body() dto: UpdateUserDto) {
-      return this.usersService.update(+id, dto);
-    }
-  
-    @Delete(':id')
-    remove(@Param('id') id: string) {
-      return this.usersService.remove(+id);
-    }
+  Controller,
+  Get,
+  Post,
+  Body,
+  Param,
+  Put,
+  Delete,
+  ParseIntPipe,
+} from '@nestjs/common';
+import { ApiTags } from '@nestjs/swagger';
+import { UsersService } from './users.service';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
+
+@ApiTags('users')
+@Controller('users')
+export class UsersController {
+  constructor(private readonly usersService: UsersService) {}
+
+  @Get()
+  findAll() {
+    return this.usersService.findAll();
   }
-  
+
+  @Get(':id')
+  findOne(@Param('id', ParseIntPipe) id: number) {
+    return this.usersService.findOne(id);
+  }
+
+  @Post()
+  create(@Body() dto: CreateUserDto) {
+    return this.usersService.create(dto);
+  }
+
+  @Put(':id')
+  update(@Param('id', ParseIntPipe) id: number, @Body() dto: UpdateUserDto) {
+    return this.usersService.update(id, dto);
+  }
+
+  @Delete(':id')
+  remove(@Param('id', ParseIntPipe) id: number) {
+    return this.usersService.remove(id);
+  }
+}
